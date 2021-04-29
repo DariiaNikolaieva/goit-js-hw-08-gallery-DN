98eeb004ceb3b6b45b9f28079b400e5cf9ee6813
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-04-29 20:02:31 +0300

    task-8

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export default [
+const images = [
   {
     preview:
       "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
@@ -63,3 +63,79 @@ export default [
     description: "Lighthouse Coast Sea",
   },
 ];
+
+// элементы
+const galleryContainer = document.querySelector(".js-gallery");
+const lightboxCloseBtn = document.querySelector(
+  '[data-action="close-lightbox"]'
+);
+const lightbox = document.querySelector(".js-lightbox");
+const lightboxImg = document.querySelector(".lightbox__image");
+const overlay = document.querySelector(".lightbox__overlay");
+
+// создание разметки
+function createImageCardsMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join("");
+}
+
+// рендер разметки
+const galleryContainerMarkup = createImageCardsMarkup(images);
+galleryContainer.insertAdjacentHTML("beforeend", galleryContainerMarkup);
+
+galleryContainer.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  // определение области клика
+  const isGalleryImageEl = event.target.classList.contains("gallery__image");
+  if (!isGalleryImageEl) {
+    return;
+  }
+  // открытиe модального окна, подмена значения атрибута src
+  lightbox.classList.add("is-open");
+  lightboxImg.src = event.target.dataset.source;
+
+  window.addEventListener("keydown", onCloseLightboxByEsc);
+}
+
+lightboxCloseBtn.addEventListener("click", onCloseLightbox);
+
+function onCloseLightbox() {
+  // закрытие модального окна
+  const openedLightbox = document.querySelector(".lightbox.is-open");
+  if (openedLightbox) {
+    openedLightbox.classList.remove("is-open");
+  }
+  lightboxImg.src = "";
+  window.removeEventListener("keydown", onCloseLightboxByEsc);
+}
+
+function onCloseLightboxByEsc(event) {
+  if (event.code === "Escape") {
+    onCloseLightbox();
+  }
+}
+
+overlay.addEventListener("click", onOverlayClick);
+
+function onOverlayClick(event) {
+  if (event.target === event.currentTarget) {
+    onCloseLightbox();
+  }
+}
