2115d8bc6d324479244fcfb9b7a6930958f99648
DariiaNikolaieva <nikolaieva.dariya@gmail.com>
2021-04-30 21:44:36 +0300

    add flip

--- index.js
+++ index.js
@@ -139,3 +139,36 @@ function onOverlayClick(event) {
     onCloseLightbox();
   }
 }
+
+// карусель
+const arrayOfImgs = galleryContainer.querySelectorAll(".gallery__image");
+// console.log(arrayOfImgs);
+const quantityOfImg = [...arrayOfImgs];
+// console.log(quantityOfImg);
+
+window.addEventListener("keydown", onPressArrow);
+
+let currentImgIndex = 0;
+
+function onRightBtnClick() {
+  if (currentImgIndex < quantityOfImg.length - 1) {
+    currentImgIndex += 1;
+    lightboxImg.src = quantityOfImg[currentImgIndex].src;
+  }
+}
+
+function onLeftBtnClick() {
+  if (currentImgIndex > 0) {
+    currentImgIndex -= 1;
+    lightboxImg.src = quantityOfImg[currentImgIndex].src;
+  }
+}
+
+function onPressArrow(event) {
+  if (event.code === "ArrowRight") {
+    onRightBtnClick();
+  }
+  if (event.code === "ArrowLeft") {
+    onLeftBtnClick();
+  }
+}
